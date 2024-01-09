be0347408d1f2956034e79acd4632059e79013a7
Victoria Arriola <arriova16@gmail.com>
2024-01-09 15:43:08 -0600

    creating formatter

---
+++ ElectDetectFormatter.m
@@ -0,0 +1,20 @@
+function output_table = ElectDetectFormatter(input_cell_array)
+
+[num_trials, num_cols] = size(input_cell_array);
+
+if num_cols == 10 
+    input_cell_array = input_cell_array(:, [1:3,5:6, 8, 10]);
+end
+
+
+%Remove aborted trials
+
+input_cell_array = input_cell_array(~strcmpi(input_cell_array(:,7), 'empty response') | ...
+                                    strcmpi(input_cell_array(:,7), 'no'), :);
+
+%converting
+
+output_table = cell2table(input_cell_array, 'VariablesNames', {'Trial', 'CorrectAnswer', 'CorrectAnserText', ...
+                                                               'StimAmp', 'StimFreq', 'Electrode', 'Response'});
+
+end
