639d690cd3b2bc21b5f2554fb01eea78be6b2eb3
Victoria Arriola <arriova16@gmail.com>
2024-01-11 00:10:07 -0600

    updates

--- Sweep_Task.m
+++
@@ -1,6 +0,0 @@
-%Working on Formatting the Sweep Detection Task
-tld = 'C:\Users\arrio\Box\BensmaiaLab\ProjectFolders\DARPA\Data\RawData\Pinot\Electrode_22and24\SweepTask\Training\SweepDetect';
-file_list = dir(data_folder);
-
-%% Loading rsp files and formatting
-
---
+++ Sweep_Task_Formatter.m
@@ -0,0 +1,50 @@
+%Working on Formatting the Sweep Detection Task
+tld = 'C:\Users\arrio\Box\BensmaiaLab\ProjectFolders\DARPA\Data\RawData\Pinot\Electrode_22and24\SweepTask\Training\SweepDetect';
+process_loc = 'C:\Users\arrio\Box\BensmaiaLab\ProjectFolders\DARPA\Data\ProcessedData\Pinot\DarpaSweep';
+file_list = dir(tld);
+
+%% Loading rsp files and formatting
+
+block_struct = struct();ii=1;
+
+for i = 1:length(file_list)
+ % Check if file is .rsp
+    if ~contains(file_list(i).name, '.rsp') || ~contains(file_list(i).name, 'darpa')
+        continue
+    end
+    
+    fname_split = strsplit(file_list(i).name, '_');
+    block_struct(ii).Animal = fname_split{2}(1:end-7);
+    monkey_name = fname_split{2}(1:end-7);
+    dt_name = datestr(datenum(fname_split{4}(1:t_idx-1), 'yyyymmdd'), 1);
+    t_idx = find(fname_split{4} == 'T');
+    block_struct(ii).Date = datestr(datenum(fname_split{4}(1:t_idx-1), 'yyyymmdd'), 1);
+
+    % load the data
+    temp = readcell(fullfile(tld, file_list(i).name), "FileType",'text', 'NumHeaderLines',1);
+     
+    if size(temp,2) == 37
+        temp = temp(:, [25:27, 29, 30, 32, 33, 34, 37]);
+
+    end
+    
+    abort_idx = strcmpi(temp(:,9), 'empty response') | strcmpi(temp(:,9), 'no');
+    temp = temp(~abort_idx,:);
+    
+    response_table = cell2table(temp, 'VariableNames',{'Trial', 'CorrectInterval', 'CorrectAnswer', ...
+                                                        'StimAmp', 'StimFreq', 'Electrode', 'IndentorFreq', ...
+                                                        'IndentorAmp', 'Response'});
+
+    block_struct(ii).ResponseTable = response_table;
+
+    ii = ii+1;
+    
+    %saving in matfiles
+   
+     fname = sprintf('%s_%s_DarpaSweep.mat', monkey_name, dt_name);
+     save(fullfile(process_loc, fname), 'response_table')
+   
+end
+
+
+
